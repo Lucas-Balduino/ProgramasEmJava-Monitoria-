8fe2e54b319e8cfeab2156ceca029c46d5b1c603
Lucas-Balduino <165683227+Lucas-Balduino@users.noreply.github.com>
2024-05-14 15:57:26 -0300

    Add files via upload

---
+++ Aula 8/Figuras Geometricas/Entidades/FiguraGeometrica.java
@@ -0,0 +1,13 @@
+package FigurasGeometricas.Entidades;
+
+public abstract class FiguraGeometrica {
+    public String nome;
+
+    public double calcArea(){
+        return 0;
+    }
+
+    public double calcPerimetro(){
+        return 0;
+    }
+}
